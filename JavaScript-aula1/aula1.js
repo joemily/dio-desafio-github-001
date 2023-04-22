0c0285b1a120408711aea0417724d9f5bbe83d4e
joemilybc <joemilybcampos@gmail.com>
2023-04-21 22:18:15 -0300

    Atualização de dados

--- JavaScript-aula1/aula1.js
+++ JavaScript-aula1/aula1.js
@@ -1,1 +1,7 @@
-console.log("Teste de Turing.")
+const precoComb = 6.35;
+const gastoMedio = 12;
+const distancia = 1324;
+
+const valorViagem = (distancia / gastoMedio) * precoComb;
+
+console.log(valorViagem);
